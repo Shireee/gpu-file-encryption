53a2307b4a316638e3356fc18cfa997eedf74112
NastaHarina <146033358+NastaHarina@users.noreply.github.com>
2024-03-29 20:11:55 +0300

    init

--- GPU_file_encryption/GPU_file_encryption/main.cpp
+++ GPU_file_encryption/GPU_file_encryption/main.cpp
@@ -5,15 +5,23 @@ int main()
 	AEScipher path("key.txt", "data");
 	
 
+	//Delete empty string
+	//path.keyss.erase(std::remove_if(path.keyss.begin(), path.keyss.end(), [](unsigned char c) { return c == '\0'; }), path.keyss.end());
+
 	std::cout << "key: ";
 	path.PrintDataFiles(path.keyss);
 
 
-
+	// Delete empty string in files
+	//for (auto& vec : path.files) {
+	//	vec.erase(std::remove(vec.begin(), vec.end(), '\0'), vec.end());
+	//}
 	
 	path.PrintDataFiles(path.files);
 
-
+	//path.files.erase(std::remove_if(path.files.begin(), path.files.end(), [](const auto& vec) {
+	//	return vec.empty();
+	//}), path.files.end());
 
 
 	std::cout << "Combined data written to output_combined.bin" << std::endl;
@@ -31,7 +39,10 @@ int main()
 		std::cout << "Data written to " << filename << std::endl;
 	}
 
+	//let the keys and files have the same data size
+	//for (size_t i = 0; i < path.files.size(); ++i) {
 
+		//std::cout << "len path.keyss " << path.keyss.size() << std::endl;
 
 	// then must be function
 	for (int i = 0; i < path.keyss.size(); i++) {
@@ -46,15 +57,13 @@ int main()
 		std::copy(path.files[i].begin(), path.files[i].end(), EncrytFiles);
 
 
-	
+		//size_t length = sizeof(EncrytData) / sizeof(EncrytData[0]);
 
 		size_t length1 = path.files[i].size();
 		std::cout << std::endl << length1 << std::endl;
 
-
-		//checkSum
-		//int sum1 = path.CheckSums(EncrytFiles, length1);
-		//std::cout << "Sum " << sum1 << std::endl;
+		int sum1 = path.CheckSums(EncrytFiles, length1);
+		std::cout << "Sum " << sum1 << std::endl;
 
 		unsigned char* EncrytData = path.EncryptionAES(EncrytFiles, Encrytkeyss);
 
@@ -67,7 +76,15 @@ int main()
 	
 
 
-
+		//std::cout << "fileEncript_check1: " << std::endl;
+		//for (auto it = path.filesEncript.begin(); it != path.filesEncript.end(); ++it) {
+		//	unsigned char* dataPtr = *it;
+		//	while (*dataPtr != '\0') {
+		//		std::cout << static_cast<int>(*dataPtr) << " ";
+		//		++dataPtr;
+		//	}
+		//	std::cout << std::endl;
+		//}
 
 		std::vector<unsigned char  > filesEncriptdata;
 		
@@ -79,7 +96,7 @@ int main()
 		}
 		std::cout << std::endl;
 
-
+		//path.filesEncript.push_back('\0');
 
 		std::vector<unsigned char> emtyLine;
 		for (unsigned char ch : filesEncriptdata) {
@@ -98,6 +115,17 @@ int main()
 
 		delete[] EncrytData;
 
+		//std::cout << "DecryptionAES: " << std::endl;
+
+		//unsigned char* DecryptionData = path.DecryptionAES(EncrytData);
+
+		//int sum2 = path.CheckSums(DecryptionData, length1);
+		//std::cout << "Sum " << sum2 << std::endl;
+
+		//for (size_t i = 0; i < length1; ++i) {
+		//	std::cout << static_cast<int>(DecryptionData[i]) << " ";
+		//}
+		//std::cout << std::endl;
 
 
 	}
@@ -105,7 +133,9 @@ int main()
 
 	path.PrintDataFiles(path.filesEncript);
 
-
+	//for (const auto& element : path.filesEncript) {
+	//	std::cout << static_cast<int>(element) << " ";
+	//}
 
 
 
