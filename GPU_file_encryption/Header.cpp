bd8ce0e0f8e8dc490173bac60591d198491c62a7
NastaHarina <146033358+NastaHarina@users.noreply.github.com>
2024-05-02 18:05:05 +0300

    Padding()

--- GPU_file_encryption/Header.cpp
+++ GPU_file_encryption/Header.cpp
@@ -4,8 +4,39 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <algorithm> // Для std::shuffle
+#include <random>    // Для std::random_device и std::mt19937
+#include "md5.h"
 
 namespace fs = std::filesystem;
+
+
+int Padding(int fileSize)
+{
+    return static_cast<size_t>(std::pow(2, std::ceil(std::log2(fileSize))));
+}
+
+
+void AEScipher::Shuffer(size_t len_key)
+{
+
+    keyss_shuffer = keyss;
+
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    std::shuffle(keyss_shuffer.begin(), keyss_shuffer.end(), g);
+
+    //// Выводим результат
+    //for (const auto& row : keyss_shuffer) {
+    //    for (const auto& element : row) {
+    //        std::cout << static_cast<int>(element) << " ";
+    //    }
+    //    std::cout << std::endl;
+    //}
+
+}
 
 // The constructor of the class. Reading files and directories
 AEScipher::AEScipher(std::string pathkey, std::string folder)
@@ -21,12 +52,19 @@ AEScipher::AEScipher(std::string pathkey, std::string folder)
         }
         else {
             emtyLine.push_back(ch);
+            
         }
     }
 
-    // Add the last line if it does not end with a newline character
     if (!emtyLine.empty()) {
         keyss.push_back(emtyLine);
+
+    }
+
+    for (int i = 0; i < keyss.size(); i++) {
+        if (keyss[i].size() % 16 != 0) {
+            PadKey(keyss[i]);
+        }
     }
 
 
@@ -45,9 +83,17 @@ AEScipher::AEScipher(std::string pathkey, std::string folder)
     }
 }
 
+void AEScipher::CheckLength(unsigned int len)
+{
+    if (len % blockBytesLen != 0) {
+        throw std::length_error("Plaintext length must be divisible by " +
+            std::to_string(blockBytesLen));
+    }
+}
+
 void AEScipher::PrintDataFiles(const std::vector<std::vector<unsigned char>>& files)
 {
-    std::cout << "Data file: " << std::endl;
+    //std::cout << "Data file: " << std::endl;
     int number_file_to_read = 0;
     for (const auto& row : files) {
         std::cout << "Data read " << number_file_to_read << " : ";
@@ -59,7 +105,7 @@ void AEScipher::PrintDataFiles(const std::vector<std::vector<unsigned char>>& fi
     }
 }
 
-void AEScipher::PrintKey(const std::vector<unsigned char>& vec)
+void AEScipher::PrintKey(const std::vector<unsigned char> vec)
 {
     std::cout << "Data in keys: ";
     for (unsigned char data : vec) {
@@ -76,16 +122,17 @@ void AEScipher::WriteFile(std::vector<unsigned char > writedata, const std::stri
         std::cerr << "Unable to open the file." << std::endl;
     }
 
-    outFile.write(reinterpret_cast<const char*>(writedata.data()), writedata.size());
+    int lastIndex = writedata.size() - 1;
+    while (lastIndex >= 0 && writedata[lastIndex] == '\0') {
+        lastIndex--;
+    }
+    if (lastIndex >= 0) {
+        outFile.write(reinterpret_cast<const char*>(writedata.data()), lastIndex + 1);
+    }
 
     outFile.close();
 }
 
-int AEScipher::CheckSums(unsigned char* data, size_t size)
-{
-
-    return 0;
-}
 
 
 
@@ -98,7 +145,17 @@ std::vector<unsigned char> AEScipher::ReadFile(std::string path) {
         return std::vector<unsigned char>();
     }
 
-    std::vector<unsigned char> buffer(64);
+    inputFile.seekg(-1, std::ios::end);
+    int size = inputFile.tellg();
+   
+    int len = Padding(size + 1);
+
+
+    std::vector<unsigned char> buffer(len);
+
+    inputFile.seekg( 0 -(size + 1), std::ios::end);
+
+
     
     inputFile.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
 
@@ -108,41 +165,117 @@ std::vector<unsigned char> AEScipher::ReadFile(std::string path) {
     return buffer;
 }
 
+void AEScipher::PadKey(std::vector<unsigned char>& addKey)
+{
+    const size_t AES_BLOCK_SIZE = 16;
+
+    // Вычисляем размер, до которого нужно дополнить ключ
+    size_t remainder = AES_BLOCK_SIZE - (addKey.size() % AES_BLOCK_SIZE);
+
+    // Дополняем ключ нулями, если это необходимо
+    if (remainder != 0) {
+        addKey.resize(addKey.size() + remainder, 0);
+    }
+}
+
+void AEScipher::CheckSumsMD5(std::vector<unsigned char> data)
+{
+
+    MD5 md5;
+
+    std::string strData(data.begin(), data.end());
+    hash.push_back( md5(strData.c_str(), strData.length()));
+
+}
 
 
-// key expansion
-void AEScipher::keyExpansion(const unsigned char* key, unsigned char w[][4][4]) {
-    int i, j, r, c;
-    unsigned char rc[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 };
 
-    // Копирование исходного ключа в w[0]
-    for (r = 0; r < 4; ++r) {
-        for (c = 0; c < 4; ++c) {
-            w[0][r][c] = key[r + c * 4];
-        }
+void AEScipher::RotWord(unsigned char* a)
+{
+    unsigned char c = a[0];
+    a[0] = a[1];
+    a[1] = a[2];
+    a[2] = a[3];
+    a[3] = c;
+}
+
+
+void AEScipher::SubWord(unsigned char* a)
+{
+    for (int i = 0; i < 4; i++) {
+        a[i] = sBox[a[i] / 16][a[i] % 16];
+    }
+}
+
+void AEScipher::Round(unsigned char* a, unsigned int n)
+{
+    unsigned int i;
+    unsigned char c = 1;
+    for (i = 0; i < n - 1; i++) {
+        c = field(c);
     }
 
-    // Генерация раундовых ключей
-    for (i = 1; i <= 10; ++i) {
-        for (j = 0; j < 4; ++j) {
-            unsigned char t[4];
-            for (r = 0; r < 4; ++r) {
-                t[r] = (j == 0) ? w[i - 1][r][3] : w[i][r][j - 1];
-            }
+    a[0] = c;
+    a[1] = a[2] = a[3] = 0;
+}
 
-            if (j == 0) {
-                unsigned char temp = t[0];
-                for (r = 0; r < 3; ++r) {
-                    t[r] = w[i][r][(r + 1) % 4];
-                }
-                t[3] = temp;
-                t[0] ^= rc[i - 1];
-            }
+unsigned char AEScipher::field(unsigned char b)
+{
+    return  (b << 1) ^ (((b >> 7) & 1) * 0x1b);
+}
 
-            for (r = 0; r < 4; ++r) {
-                w[i][r][j] = w[i - 1][r][j] ^ t[r];
-            }
+void AEScipher::InvSbox()
+{
+
+    for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 16; ++j) {
+            unsigned char original_value = sBox[i][j];
+            invSbox[original_value / 16][original_value % 16] = i * 16 + j;
         }
+    }
+}
+
+void AEScipher::Xor(unsigned char* a, unsigned char* b, unsigned char* c)
+{
+    int i;
+    for (i = 0; i < 4; i++) {
+        c[i] = a[i] ^ b[i];
+    }
+}
+
+// key expansion
+void AEScipher::keyExpansion(const unsigned char key[], unsigned char w[]) {
+    unsigned char temp[4];
+    unsigned char rcon[4];
+
+    size_t i = 0;
+    while (i < 4 * Nk) {
+        w[i] = key[i];
+        i++;
+    }
+
+    i = 4 * Nk;
+    while (i < 4 * Nb * (Nr + 1)) {
+        temp[0] = w[i - 4 + 0];
+        temp[1] = w[i - 4 + 1];
+        temp[2] = w[i - 4 + 2];
+        temp[3] = w[i - 4 + 3];
+
+        if (i / 4 % Nk == 0) {
+            RotWord(temp);
+            SubWord(temp);
+            Round(rcon, i / (Nk * 4));
+            Xor(temp, rcon, temp);
+        }
+        else if (Nk > 6 && i / 4 % Nk == 4) {
+            SubWord(temp);
+        }
+
+        w[i + 0] = w[i - 4 * Nk] ^ temp[0];
+        w[i + 1] = w[i + 1 - 4 * Nk] ^ temp[1];
+        w[i + 2] = w[i + 2 - 4 * Nk] ^ temp[2];
+        w[i + 3] = w[i + 3 - 4 * Nk] ^ temp[3];
+        i += 4;
     }
 }
 
@@ -170,13 +303,27 @@ unsigned char AEScipher::ffMultiply(unsigned char a, unsigned char b) {
 }
 
 // replacing bytes in the status matrix
-void AEScipher::subBytes(unsigned char state[][4], const unsigned char* sBox) {
-    int r, c;
-    for (r = 0; r < 4; ++r) {
-        for (c = 0; c < 4; ++c) {
-            state[r][c] = sBox[state[r][c]];
+void AEScipher::subBytes(unsigned char state[4][Nb]) {
+    unsigned char s;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < Nb; j++) {
+            s = state[i][j];
+            state[i][j] = sBox[s / 16][s % 16];
         }
     }
+}
+
+void AEScipher::InvsubBytes(unsigned char state[4][Nb])
+{
+    unsigned char s;
+    InvSbox();
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < Nb; j++) {
+            s = state[i][j];
+            state[i][j] = invSbox[s / 16][s % 16];
+        }
+    }
+
 }
 
 // cyclic shift of rows in the state matrix
@@ -194,21 +341,33 @@ void AEScipher::shiftRows(unsigned char state[][4]) {
 }
 
 // line shift
-void AEScipher::InvShiftRows(unsigned char state[][4]) {
-    unsigned char t[4];
-    int r, c;
-    for (r = 1; r < 4; ++r) {
-        for (c = 0; c < 4; ++c) {
-            t[(c + r) % 4] = state[r][c];
-        }
-        for (c = 0; c < 4; ++c) {
-            state[r][c] = t[c];
-        }
+void AEScipher::ShiftRows(unsigned char state[4][Nb]) {
+
+    ShiftRow(state, 1, 1);
+    ShiftRow(state, 2, 2);
+    ShiftRow(state, 3, 3);
+
+}
+
+void AEScipher::ShiftRow(unsigned char state[4][Nb], unsigned int i, unsigned int n)
+{
+    unsigned char tmp[Nb];
+    for (unsigned int j = 0; j < Nb; j++) {
+        tmp[j] = state[i][(j + n) % Nb];
     }
+    memcpy(state[i], tmp, Nb * sizeof(unsigned char));
+
+}
+
+void AEScipher::InvShiftRows(unsigned char state[4][Nb])
+{
+    ShiftRow(state, 1, Nb - 1);
+    ShiftRow(state, 2, Nb - 2);
+    ShiftRow(state, 3, Nb - 3);
 }
 
 // shuffling columns in the state matrix
-void AEScipher::mixColumns(unsigned char state[][4]) {
+void AEScipher::mixColumns(unsigned char state[4][Nb]) {
     unsigned char t[4];
     int r, c;
     for (c = 0; c < 4; ++c) {
@@ -242,117 +401,282 @@ void AEScipher::InvMixColumns(unsigned char state[][4]) {
 }
 
 // performing the XOR operation on the round state and key matrices
-void AEScipher::addRoundKey(unsigned char state[][4], unsigned char k[][4]) {
-    int r, c;
-    for (c = 0; c < 4; ++c) {
-        for (r = 0; r < 4; ++r) {
-            state[r][c] ^= k[r][c];
+void AEScipher::addRoundKey(unsigned char state[4][Nb], unsigned char* key) {
+    for (int c = 0; c < 4; ++c) {
+        for (int r = 0; r < 4; ++r) {
+            state[r][c] ^= key[c + 4 * r];
         }
     }
 }
 
 // data block encryption
-void AEScipher::cipher(unsigned char* input, unsigned char w[][4][4], const unsigned char* sBox) {
-    unsigned char state[4][4];
-    int i, r, c;
+void AEScipher::EncryptBlock(const unsigned char input[], unsigned char out[], unsigned char* roundKeys) {
 
-    for (r = 0; r < 4; ++r) {
-        for (c = 0; c < 4; ++c) {
-            state[r][c] = input[c * 4 + r];
+    unsigned char state[4][Nb];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < Nb; ++j) {
+            state[i][j] = input[i + 4 * j];
         }
     }
 
-    addRoundKey(state, w[0]);
+    addRoundKey(state, roundKeys);
 
-    for (i = 1; i <= 10; ++i) {
-        subBytes(state, sBox);
+    int round;
+
+    for (round = 1; round <= Nr - 1; round++) {
+        subBytes(state);
         shiftRows(state);
-        if (i != 10) mixColumns(state);
-        addRoundKey(state, w[i]);
+        mixColumns(state);
+        addRoundKey(state, roundKeys + round * 4 * Nb);
     }
 
-    for (r = 0; r < 4; ++r) {
-        for (c = 0; c < 4; ++c) {
-            input[c * 4 + r] = state[r][c];
+    subBytes(state);
+    shiftRows(state);
+    addRoundKey(state, roundKeys + Nr * 4 * Nb);
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            out[i + 4 * j] = state[i][j];
         }
     }
+
 }
 
-// inverted S-Box transformation
+// inverted S-Box transformation  сheck
 void invertSBox(const unsigned char* sBox, unsigned char* invSBox) {
-    for (int i = 0; i < 16; ++i) {
-        for (int j = 0; j < 16; ++j) {
-            invSBox[sBox[i * 16 + j]] = static_cast<unsigned char>(i * 16 + j);
-        }
+    for (int i = 0; i < 256; ++i) {
+        invSBox[sBox[i]] = static_cast<unsigned char>(i);
     }
 }
 
-void AEScipher::InvCipher(unsigned char* input, unsigned char* keyN, const unsigned char* invSBox) {
-    unsigned char state[4][4];
-    int i, r, c;
+void AEScipher::DecryptionBlock(const unsigned char input[], unsigned char output[],
+    unsigned char* roundKeys) {
+    unsigned char state[4][Nb];
+    
 
-    keyExpansion(keyN, w);
+    //keyExpansion(keyN, w);
 
-    for (r = 0; r < 4; ++r) {
-        for (c = 0; c < 4; ++c) {
+
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < Nb; c++) {
             state[r][c] = input[c * 4 + r];
         }
     }
 
-    addRoundKey(state, w[10]);
+    addRoundKey(state, roundKeys + Nr * 4 * Nb);
 
-    for (i = 9; i >= 0; --i) {
+    int round;
+
+    for (round = Nr-1; round >= 1; round--) {
+        InvsubBytes(state);
         InvShiftRows(state);
-        subBytes(state, invSBox);
-        addRoundKey(state, w[i]);
-        if (i != 0) InvMixColumns(state);
+        addRoundKey(state, roundKeys + round * 4 * Nb);
+
+       /* subBytes(state, invSBox);*/
+        
+        InvMixColumns(state);
     }
 
-    for (r = 0; r < 4; ++r) {
-        for (c = 0; c < 4; ++c) {
-            input[c * 4 + r] = state[r][c];
+    InvsubBytes(state);
+    InvShiftRows(state);
+    addRoundKey(state, roundKeys + round * 4 * Nb);
+
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < Nb; c++) {
+            output[c * 4 + r] = state[r][c];
         }
     }
 }
 
 
-unsigned char*AEScipher::EncryptionAES(unsigned char* fileEn, unsigned char* keyEn)
+unsigned char* AEScipher::EncryptionAES(const unsigned char fileEn[], unsigned int fileLen,
+    const unsigned char keyEn [])
 {
 
-    unsigned char* output;
-    unsigned char iSBox[256];
+    unsigned char* EncryptText = new unsigned char[fileLen];
+    unsigned char* roundKeys = new unsigned char[4 * Nb * (Nr + 1)];
 
-    //invertSBox(sBox, iSBox);
-    keyExpansion(keyEn, w);
+    // Выполняем ключевое расширение
+    keyExpansion(keyEn, roundKeys);
 
-    for (size_t i = 0; i < 4; ++i) {
-        unsigned char* block = fileEn + i * 16;
-        cipher(block, w, sBox);
+    // Шифруем каждый блок данных
+    for (size_t i = 0; i < fileLen; i+= blockBytesLen) {
+        EncryptBlock(fileEn + i, EncryptText + i, roundKeys); // Вызываем функцию шифрования блока данных
     }
-    
-    return fileEn;
 
+    delete[] roundKeys;
+
+    return EncryptText;
+}
+
+void AEScipher::StartEncryption()
+{
+    for (int i = 0; i < keyss.size(); i++) {
+
+        unsigned char* Encrytkeyss = new unsigned char[keyss[i].size()];
+        unsigned char* EncrytFiles = new unsigned char[files[i].size()];
+
+        std::copy(keyss[i].begin(), keyss[i].end(), Encrytkeyss);
+        std::copy(files[i].begin(), files[i].end(), EncrytFiles);
+
+
+
+
+        size_t length1 = files[i].size();
+
+
+
+        unsigned char* EncrytData = EncryptionAES(EncrytFiles, length1, Encrytkeyss);
+
+
+
+        std::vector<unsigned char > filesEncriptdata;
+
+
+        for (size_t j = 0; j < length1; ++j) {
+            filesEncriptdata.push_back(EncrytData[j]);
+            //std::cout << static_cast<int>(EncrytData[j]) << " ";
+        }
+        std::cout << "Encript file " << i << std::endl;
+
+
+        filesEncript.push_back(filesEncriptdata);
+
+        std::string filename = "dataEncode/fileEncript" + std::to_string(i) + ".bin";
+        WriteFile(filesEncriptdata, filename);
+        std::cout << "Data written to " << filename << std::endl << std::endl;
+
+        //std::vector<unsigned char> read_Encript_file = path.ReadFile("dataEncode/fileEncript" + std::to_string(i) + ".bin");
+
+        //path.PrintKey(read_Encript_file);
+
+
+        delete[] EncrytData;
+
+    }
 }
 
 
 
-unsigned char* AEScipher::DecryptionAES(unsigned char* fileEn, unsigned char* keyEn)
+
+unsigned char* AEScipher::DecryptionAES(const unsigned char fileDe[], unsigned int fileLen,
+    const unsigned char keyDe[])
 {
-    if (keyss.empty() || files.empty()) {
-        std::cout << "In function"<<std::endl;
-        PrintDataFiles(keyss);
+    //CheckLength(fileLen);
 
-        std::cerr << "Key or file data is missing." << std::endl;
-        return nullptr;
+    unsigned char* DecryptionText = new unsigned char[fileLen];
+    unsigned char* roundKeys = new unsigned char[4 * Nb * (Nr + 1)];
+
+    // Выполняем ключевое расширение
+    keyExpansion(keyDe, roundKeys);
+
+    // Шифруем каждый блок данных
+    for (size_t i = 0; i < fileLen; i += blockBytesLen) {
+        DecryptionBlock(fileDe + i, DecryptionText + i, roundKeys); // Вызываем функцию шифрования блока данных
     }
 
-    unsigned char iSBox[256];
-    invertSBox(sBox, iSBox);
+    delete[] roundKeys;
 
-    for (size_t i = 0; i < 4; ++i) {
-        unsigned char* block = fileEn + i * 16;
-        InvCipher(block, keyEn, iSBox);
-    }
-
-    return fileEn;
+    return DecryptionText;
 }
+
+void AEScipher::StartDecryption()
+{
+    for (int i = 0; i < keyss.size(); i++) {
+
+
+
+        unsigned char* Decryptkeyss = new unsigned char[keyss[i].size()];
+        unsigned char* DecryptFiles = new unsigned char[filesEncript[i].size()];
+
+        std::copy(keyss[i].begin(), keyss[i].end(), Decryptkeyss);
+        std::copy(filesEncript[i].begin(), filesEncript[i].end(), DecryptFiles);
+
+        size_t length1 = filesEncript[i].size();
+
+
+
+        unsigned char* DecryptionData = DecryptionAES(DecryptFiles, length1, Decryptkeyss);
+        
+
+        std::vector<unsigned char > filesDecryptdata;
+
+
+        for (size_t j = 0; j < length1; ++j) {
+            filesDecryptdata.push_back(DecryptionData[j]);
+            //std::cout << static_cast<int>(DecryptionData[j]) << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Decrypt file " << i << std::endl;
+
+        filesDescript.push_back(filesDecryptdata);
+
+        std::string filename = "dataDecrypt/fileDecrypt" + std::to_string(i) + ".bin";
+        WriteFile(filesDecryptdata, filename);
+        std::cout << "Data written to " << filename << std::endl << std::endl;
+
+        //std::vector<unsigned char> read_Decrypt_file = ReadFile(filename);
+
+
+
+        delete[] DecryptionData;
+    }
+}
+
+void AEScipher::StartDecryptionShuffer()
+{
+
+    for (int i = 0; i < filesEncript.size(); i++) {
+        for (int j = 0; j < keyss_shuffer.size(); j++) {
+
+            unsigned char* Decryptkeyss = new unsigned char[keyss_shuffer[j].size()];
+            unsigned char* DecryptFiles = new unsigned char[filesEncript[i].size()];
+
+            std::copy(keyss_shuffer[j].begin(), keyss_shuffer[j].end(), Decryptkeyss);
+            std::copy(filesEncript[i].begin(), filesEncript[i].end(), DecryptFiles);
+
+            size_t length1 = filesEncript[i].size();
+
+
+
+
+            unsigned char* DecryptionData = DecryptionAES(DecryptFiles, length1, Decryptkeyss);
+
+
+            std::vector<unsigned char> tempData(DecryptionData, DecryptionData + length1);
+
+            std::string strData(tempData.begin(), tempData.end());
+
+            MD5 md5;
+
+            std::string hash_check = md5(strData.c_str(), strData.length());
+
+            if (hash_check == hash[i]) {
+                std::cout << "Decrypt file " << i << std::endl;
+
+                std::vector<unsigned char > filesDecryptdata;
+
+
+                // check DecryptionData
+                for (size_t k = 0; k < length1; ++k) {
+                    filesDecryptdata.push_back(DecryptionData[k]);
+                    //std::cout << static_cast<int>(DecryptionData[k]) << " ";
+                }
+
+
+
+
+                std::string filename = "dataDecrypt/fileDecrypt" + std::to_string(i) + ".bin";
+                WriteFile(filesDecryptdata, filename);
+                std::cout << "Data written to " << filename << std::endl<<std::endl;
+
+                //std::vector<unsigned char> read_Decrypt_file = ReadFile(filename);
+            }
+
+        }
+    }
+}
+
+
--- GPU_file_encryption/main.cpp
+++ GPU_file_encryption/main.cpp
@@ -1,172 +1,83 @@
+п»ї
 #include "Header.h"
+#include "md5.h"
+
+
 
 int main()
 {
+
+
 	AEScipher path("key.txt", "data");
-	
+	bool readKey = false;
+	std::cout << "Output the keys?\n1 - Yes 0 - No : " << std::endl;
+	std::cin >> readKey;
+	if (readKey == 1) {
+		std::cout<<"Key:" << std::endl;
 
-	std::cout << "key: ";
-	path.PrintDataFiles(path.keyss);
-
-
-
-	
-	path.PrintDataFiles(path.files);
-
-
-
-
-	std::cout << "Combined data written to output_combined.bin" << std::endl;
-
-
-	path.files.erase(std::remove_if(path.files.begin(), path.files.end(), [](const auto& vec) {
-		return vec.empty();
-		}), path.files.end());
-
-	std::cout << "Writing data to a files: "<<std::endl;
-
-	for (std::size_t i = 0; i < path.files.size(); ++i) {
-		std::string filename = "dataEncode/fileEncript" + std::to_string(i) + ".bin";
-		path.WriteFile(path.files[i], filename);
-		std::cout << "Data written to " << filename << std::endl;
+		path.PrintDataFiles(path.keyss);
 	}
 
 
 
-	// then must be function
-	for (int i = 0; i < path.keyss.size(); i++) {
-
-		// 
-		
-
-		unsigned char* Encrytkeyss = new unsigned char[path.keyss[i].size()];//path.EncryptionAES(path.files[3].data(), path.keyss[3].data());
-		unsigned char* EncrytFiles = new unsigned char[path.files[i].size()];
-
-		std::copy(path.keyss[i].begin(), path.keyss[i].end(), Encrytkeyss);
-		std::copy(path.files[i].begin(), path.files[i].end(), EncrytFiles);
-
-
+	for (int i = 0; i < path.files.size(); i++) {
+		path.CheckSumsMD5(path.files[i]);
+	}
+	std::cout << std::endl;
 	
-
-		size_t length1 = path.files[i].size();
-		std::cout << std::endl << length1 << std::endl;
-
-
-		//checkSum
-		//int sum1 = path.CheckSums(EncrytFiles, length1);
-		//std::cout << "Sum " << sum1 << std::endl;
-
-		unsigned char* EncrytData = path.EncryptionAES(EncrytFiles, Encrytkeyss);
-
-		std::cout<<"strlen "<<strlen(reinterpret_cast<const char*>(EncrytData))<<std::endl;
-
-
-
-
-
 	
 
 
+	if (path.keyss.size() == path.files.size()) {
+		std::cout << "Equal number of keys and files" << std::endl;
+
+		path.StartEncryption();
 
 
-		std::vector<unsigned char  > filesEncriptdata;
-		
+		std::cout << std::endl << std::endl;
 
-		std::cout << "EncryptionAES: " << std::endl;
-		for (size_t  j= 0; j < length1; ++j) {
-			filesEncriptdata.push_back(EncrytData[j]);
-			std::cout << static_cast<int>(EncrytData[j]) << " ";
-		}
+		// new keyss in keyss_shuffer
+		path.Shuffer(path.keyss.size());
+
+		std::cout << "0 - The keys go sequentially to the files\n"
+			"1 - The order of the keys is unknown: ";
+
+		bool index;
+
+		std::cin >> index;
+
 		std::cout << std::endl;
 
-
-
-		std::vector<unsigned char> emtyLine;
-		for (unsigned char ch : filesEncriptdata) {
-			if (ch == '\n') {
-				path.filesEncript.push_back(emtyLine);
-				emtyLine.clear();
-			}
-			else {
-				emtyLine.push_back(ch);
-			}
+		if (index == 0) {
+			path.StartDecryption();
 		}
-		if (!emtyLine.empty()) {
-			path.filesEncript.push_back(emtyLine);
+		else if (index == 1) {
+			path.StartDecryptionShuffer();
 		}
-
-
-		delete[] EncrytData;
-
-
+		else {
+			std::cout << "Incorrect data entered" << std::endl;
+		}
 
 	}
-	std::cout << "Contents of dataVector:" << std::endl;
-
-	path.PrintDataFiles(path.filesEncript);
-
-
-
-
-
-
-
-
-
-
-	int length1E = path.filesEncript.size();
-	std::cout << "key CHECKK" << std::endl;
-
-	path.PrintDataFiles(path.keyss);
-
-
-
-
-	//del
-	 
-	 
-	for (int i = 0; i < path.keyss.size(); i++) {
-		unsigned char* Decryptkeyss = new unsigned char[path.keyss[i].size()];//path.EncryptionAES(path.files[3].data(), path.keyss[3].data());
-		unsigned char* DecryptFiles = new unsigned char[path.filesEncript[i].size()];
-
-		std::copy(path.keyss[i].begin(), path.keyss[i].end(), Decryptkeyss);
-		std::copy(path.filesEncript[i].begin(), path.filesEncript[i].end(), DecryptFiles);
-
-		size_t length1 = path.filesEncript[i].size();
-
-		std::cout << "path.keyss[i]" << std::endl;
-		for (const auto& element : path.keyss[i]) {
-
-			std::cout << static_cast<int>(element) << " ";
-		}
-		//std::cout <<"Encrytkeyss: "<< i<<" " << Decryptkeyss << std::endl;
-	
-
-		unsigned char* DecryptionData = path.DecryptionAES(DecryptFiles, Decryptkeyss);
-		std::cout << "DecryptionAEScheck: " << std::endl;
-
-
-
-		std::cout << "DecryptionData[" << i <<"] " << std::endl;
-		for (size_t j = 0; j < length1; ++j) {
-			std::cout << static_cast<int>(DecryptionData[j]) << " ";
-		}
-		std::cout << std::endl;
+	else {
+		std::cout << "Error: not equal number of keys and files" << std::endl;
 		
 	}
+	
+
 
 	for (auto& vec : path.keyss) {
-		vec.clear(); // Очищаем содержимое вложенных векторов
+		vec.clear(); 
 	}
 	path.keyss.clear();
 
 	for (auto& vec : path.keyss) {
-		vec.clear(); // Очищаем содержимое вложенных векторов
+		vec.clear(); 
 	}
 	path.keyss.clear();
+
 
 
 
 	return 0;
 }
-
